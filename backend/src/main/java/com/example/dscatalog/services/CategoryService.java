0a2b932cb062f982724e969f5a1a757adf38bd72
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-10-01 08:17:22 -0300

    H2 database and layers

---
+++ backend/src/main/java/com/example/dscatalog/services/CategoryService.java
@@ -0,0 +1,19 @@
+package com.example.dscatalog.services;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.dscatalog.entities.Category;
+import com.example.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+}
