9fe4b44ae4ffcff8cdcfbf81a030d439ab543c53
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-10-01 18:30:49 -0300

    Post category

--- backend/src/main/java/com/example/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/example/dscatalog/services/CategoryService.java
@@ -1,4 +1,5 @@
 package com.example.dscatalog.services;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -11,25 +12,38 @@ import com.example.dscatalog.entities.Category;
 import com.example.dscatalog.repositories.CategoryRepository;
 import com.example.dscatalog.services.exceptions.ResourceNotFoundException;
 
-
 @Service
 public class CategoryService {
 
     @Autowired
     private CategoryRepository categoryRepository;
 
-    @Transactional(readOnly=true)
+    @Transactional(readOnly = true)
     public List<CategoryDTO> findAll() {
         List<Category> list = categoryRepository.findAll();
         List<CategoryDTO> result = list.stream().map(cat -> new CategoryDTO(cat)).toList();
         return result;
     }
 
-    @Transactional(readOnly=true)
+    @Transactional(readOnly = true)
     public CategoryDTO findById(Long id) {
         Optional<Category> result = categoryRepository.findById(id);
         Category category = result.orElseThrow(() -> new ResourceNotFoundException("Resource not found!"));
         CategoryDTO dto = new CategoryDTO(category);
         return dto;
     }
+
+    @Transactional
+    public CategoryDTO insert(CategoryDTO dto) {
+        Category entity = new Category();
+        copyDtoToEntity(dto, entity);
+        entity = categoryRepository.save(entity);
+        return new CategoryDTO(entity);
+    }
+
+    private void copyDtoToEntity(CategoryDTO dto, Category entity) {
+        entity.setName(dto.getName());
+    }
+
 }
+
