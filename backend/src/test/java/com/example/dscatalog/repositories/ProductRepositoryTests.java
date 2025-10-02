48460c117edb4b689e3f62050eb4b904f15351d1
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-10-02 20:40:36 -0300

    BeforeEach

--- backend/src/test/java/com/example/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/example/dscatalog/repositories/ProductRepositoryTests.java
@@ -2,6 +2,7 @@ package com.example.dscatalog.repositories;
 
 import com.example.dscatalog.entities.Product;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -11,19 +12,21 @@ import java.util.Optional;
 @DataJpaTest
 public class ProductRepositoryTests {
 
+    private long existingId;
+
     @Autowired
     private ProductRepository productRepository;
 
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+    }
+
     @Test
     public void deleteShouldDeleteObjectWhenIdExists() {
-        // Arrange
-        long existingId = 1L;
 
-        // Act
         productRepository.deleteById(existingId);
         Optional<Product> result = productRepository.findById(existingId);
-
-        // Assert
         Assertions.assertFalse(result.isPresent());
     }
 }
