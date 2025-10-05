77418b99fd73fd225120a067a6774bda498d7567
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-10-05 10:46:34 -0300

    Mocking other behaviors

--- backend/src/test/java/com/example/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/example/dscatalog/services/ProductServiceTests.java
@@ -1,15 +1,22 @@
 package com.example.dscatalog.services;
 
+import com.example.dscatalog.entities.Product;
 import com.example.dscatalog.repositories.ProductRepository;
 import com.example.dscatalog.services.exceptions.DatabaseException;
 import com.example.dscatalog.services.exceptions.ResourceNotFoundException;
+import com.example.dscatalog.tests.factory.Factory;
 import org.junit.jupiter.api.*;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import com.example.dscatalog.services.exceptions.ResourceNotFoundException;
+
+import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -23,17 +30,25 @@ public class ProductServiceTests {
     private long existingId;
     private long notExistingId;
     private long dependentId;
+    private PageImpl<Product> page; // Tipo concreto que representa uma página de dados
+    private Product product;
 
     @BeforeEach
     void setUp() throws Exception {
         existingId = 1L;
         notExistingId = 1000L;
         dependentId = 3L;
+        product = Factory.createProduct();
+        page = new PageImpl<>(List.of(product));
         Mockito.when(productRepository.existsById(existingId)).thenReturn(true);
         Mockito.when(productRepository.existsById(notExistingId)).thenReturn(false);
         Mockito.when(productRepository.existsById(dependentId)).thenReturn(true);
 
         Mockito.doThrow(DataIntegrityViolationException.class).when(productRepository).deleteById(dependentId);
+        Mockito.when(productRepository.findAll((Pageable) ArgumentMatchers.any())).thenReturn(page);
+        Mockito.when(productRepository.save(ArgumentMatchers.any())).thenReturn(product); // Similar inclusão de um produto
+        Mockito.when(productRepository.findById(existingId)).thenReturn(Optional.of(product)); // Simular retorno de um id existente
+        Mockito.when(productRepository.findById(notExistingId)).thenReturn(Optional.empty()); // Simular retorn de um id não existente
     }
 
     @Test
