a35179e4704713d909fa8cde52d09854ad1bf642
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-10-08 15:32:00 -0300

    UserDetailsService

--- backend/src/main/java/com/example/dscatalog/services/UserService.java
+++ backend/src/main/java/com/example/dscatalog/services/UserService.java
@@ -6,6 +6,7 @@ import com.example.dscatalog.dto.UserInsertDTO;
 import com.example.dscatalog.dto.UserUpdateDTO;
 import com.example.dscatalog.entities.Role;
 import com.example.dscatalog.entities.User;
+import com.example.dscatalog.projections.UserDetailsProjection;
 import com.example.dscatalog.repositories.RoleRepository;
 import com.example.dscatalog.repositories.UserRepository;
 import com.example.dscatalog.services.exceptions.DatabaseException;
@@ -14,15 +15,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
@@ -75,6 +80,24 @@ public class UserService {
         }
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<UserDetailsProjection> result = userRepository.searchUserAndRolesByEmail(username);
+
+        if (result.isEmpty()) {
+            throw new UsernameNotFoundException("User not found!");
+        }
+        User user = new User();
+        user.setEmail(username);
+        user.setPassword(result.get(0).getPassword());
+
+        for(UserDetailsProjection projection:result) {
+            user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+        }
+
+        return user;
+    }
+
     private void copyDtoToEntity(UserDTO dto, User entity) {
         entity.setFirstName(dto.getFirstName());
         entity.setLastName(dto.getLastName());
@@ -86,4 +109,5 @@ public class UserService {
             entity.getRoles().add(role);
         }
     }
+
 }
