a33f305f322c47cdae2843a44b81418bea488e22
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-10-08 15:25:30 -0300

    GrantedAuthority and UserDetails

--- backend/src/main/java/com/example/dscatalog/entities/User.java
+++ backend/src/main/java/com/example/dscatalog/entities/User.java
@@ -1,14 +1,14 @@
 package com.example.dscatalog.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -19,7 +19,7 @@ public class User {
     private String email;
     private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany
     @JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private final Set<Role> roles = new HashSet<>();
 
@@ -65,16 +65,26 @@ public class User {
         this.email = email;
     }
 
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+
+    public boolean hasRole(String roleName) {
+        for(Role role: roles) {
+            if(role.getAuthority().equals(roleName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
     }
 
     @Override
@@ -88,4 +98,19 @@ public class User {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
 }
